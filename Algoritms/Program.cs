2cb92904e7e31a923516156a949de5b9681561f5
lexxA87 <alex.o.nikonov@gmail.com>
2022-10-29 19:01:50 +0400

    Added Sort class and SelectionSort

--- Algoritms/Program.cs
+++ Algoritms/Program.cs
@@ -5,15 +5,20 @@
         public static void Main()
         {
             Console.WriteLine("Yooo!");
-            var arr = new int[] { 1, 3, 5, 3, 0, 9 };
+            var arr = new int[] { 1, 3, 5, 3, 0, 9, 10, 2, 4, 42 };
             var arr2 = new int[] { 1, 3, 5, 6, 10, 15 };
             //Console.WriteLine(LinearSearch.SimpleLinearSearch(arr, 5));
             //Console.WriteLine(LinearSearch.BetterLinearSearch(arr, 7));
             //Console.WriteLine(LinearSearch.SentinelLinearSearch(arr, 3));
             //Console.WriteLine(LinearSearch.Factorial(10));
             //Console.WriteLine(LinearSearch.RecursiveLinearSearch(arr, 7));
-            Console.WriteLine(BinarySearchs.BinarySearch(arr2, 1));
-            Console.WriteLine(BinarySearchs.RecursiveBinarySearch(arr2, 42, 0, arr2.Length - 1));
+            //Console.WriteLine(BinarySearchs.BinarySearch(arr2, 1));
+            //Console.WriteLine(BinarySearchs.RecursiveBinarySearch(arr2, 42, 0, arr2.Length - 1));
+            var arr3 = Sort.SelectionSort(arr);
+            var arr4 = Sort.SelectionSort(Array.Empty<int>());
+            foreach (int i in arr3) Console.Write(i + " ");
+            foreach (int i in arr4) Console.Write(i + " ");
+
         }
     }
 }
---
+++ Algoritms/Sort.cs
@@ -0,0 +1,19 @@
+﻿namespace Algoritms
+{
+    public class Sort
+    {
+        public static int[] SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int smallest = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[smallest]) smallest = j;
+                }
+                (array[smallest], array[i]) = (array[i], array[smallest]);
+            }
+            return array;
+        }
+    }
+}
